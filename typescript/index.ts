fdd72ed98191fdcc868a48478a121c2725fcdd0d
Mykle23-AllInOne-Hereda <mykle23.dev@gmail.com>
2024-12-17 17:01:53 +0100

    tipos variables y annotations en typescript

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const url = "https://jsonplaceholder.typicode.com/todos/1";
-
 interface Todo {
   id: number;
   title: string;
@@ -13,9 +12,13 @@ axios.get(url).then((response) => {
   const title = todo.title;
   const completed = todo.completed;
 
+  logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
   console.log(`
     The Todo with ID ${id} 
     Has a title of: ${title}
     Is it finished? ${completed}
     `);
-});
+};
