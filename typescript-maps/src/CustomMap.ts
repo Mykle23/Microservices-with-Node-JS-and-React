475d08c1828f39f2fd50bd924e675015c6cf36be
Mykle23-AllInOne-Hereda <mykle23.dev@gmail.com>
2024-12-18 15:53:33 +0100

    Corrige inmportacion de referencia de google.maps

--- typescript-maps/src/CustomMap.ts
+++ typescript-maps/src/CustomMap.ts
@@ -1,3 +1,5 @@
+/// <reference types="@types/google.maps" />
+
 import { Company } from './Company';
 import { User } from './User';
 
--- typescript-maps/src/index.ts
+++ typescript-maps/src/index.ts
@@ -1,12 +1,10 @@
-/// <reference types="@types/google.maps" />
-
-import { Company } from "./Company";
-import { User } from "./User";
-import { CustomMap } from "./CustomMap";
+import { Company } from './Company';
+import { User } from './User';
+import { CustomMap } from './CustomMap';
 
 const user = new User();
 const company = new Company();
-const customMap = new CustomMap("map");
+const customMap = new CustomMap('map');
 
 customMap.addMarker(user);
 customMap.addMarker(company);
