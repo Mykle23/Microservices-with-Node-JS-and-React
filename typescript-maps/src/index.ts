917a6417ae162014a1ac8ff792d6f38d08802b13
Mykle23-AllInOne-Hereda <mykle23.dev@gmail.com>
2024-12-18 15:31:10 +0100

    Mapa para google maps

--- typescript-maps/src/index.ts
+++ typescript-maps/src/index.ts
@@ -3,12 +3,10 @@
 import { Company } from "./Company";
 import { User } from "./User";
 import { CustomMap } from "./CustomMap";
-// const user = new User();
 
-// console.log(user);
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap("map");
 
-// const company = new Company();
-
-// console.log(company);
-
-new CustomMap("map");
+customMap.addMarker(user);
+customMap.addMarker(company);
