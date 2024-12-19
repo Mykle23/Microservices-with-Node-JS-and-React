c613b40f1184dff0dce9a20951092e3c71b61d54
Mykle23-AllInOne-Hereda <mykle23.dev@gmail.com>
2024-12-19 12:14:03 +0100

    Custom error message throw err

--- ticketing/auth/src/errors/custom-error.ts
+++ ticketing/auth/src/errors/custom-error.ts
@@ -1,7 +1,7 @@
 export abstract class CustomError extends Error {
   abstract statusCode: number;
 
-  constructor() {
+  constructor(message: string) {
     super();
 
     Object.setPrototypeOf(this, CustomError.prototype);
