15eda5feae929ae005cc980ab7bf09213c4b2a91
Mykle23-AllInOne-Hereda <mykle23.dev@gmail.com>
2024-12-19 10:55:59 +0100

    error handler

--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { DatabaseConnectionError } from '../errors/database-connection-error';
+
 export const errorHandler = (
   err: Error,
   req: Request,
@@ -8,14 +9,18 @@ export const errorHandler = (
   next: NextFunction
 ) => {
   if (err instanceof RequestValidationError) {
-    console.log('handling this error as a request validation error');
+    const formattedErrors = err.error.map((error) => {
+      if (error.type === 'field') {
+        return { message: error.msg, field: error.path };
+      }
+    });
+    return res.status(400).send({ errors: formattedErrors });
   }
-
   if (err instanceof DatabaseConnectionError) {
-    console.log('handling this error as a db connection error');
+    return res.status(500).send({ errors: [{ message: err.reason }] });
   }
 
   res.status(400).send({
-    message: err.message,
+    errors: [{ message: 'Something went wrong' }],
   });
 };
