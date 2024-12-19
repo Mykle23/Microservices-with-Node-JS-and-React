bd948b8bf5ee3780c151fcb0e9cdb75a481540fb
Mykle23-AllInOne-Hereda <mykle23.dev@gmail.com>
2024-12-19 11:51:04 +0100

    Dos formas de usar implementar CustomError, (interfaz y clase abstracta)

--- ticketing/auth/src/errors/request-validation-error.ts
+++ ticketing/auth/src/errors/request-validation-error.ts
@@ -1,9 +1,27 @@
 import { ValidationError } from 'express-validator';
-export class RequestValidationError extends Error {
+
+interface CustomError {
+  statusCode: number;
+  serializeErrors(): {
+    message: string;
+    field?: string;
+  }[];
+}
+
+export class RequestValidationError extends Error implements CustomError {
+  statusCode = 400;
   constructor(public error: ValidationError[]) {
     super();
 
     // Only because we are extending a built in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+  serializeErrors() {
+    return this.error.map((err) => {
+      if (err.type === 'field') {
+        return { message: err.msg, field: err.path };
+      }
+      return { message: err.msg };
+    });
   }
 }
